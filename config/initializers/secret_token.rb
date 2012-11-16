7c299b3cf0e7a29627f2476c1b237bc858a5894c
raymac2020 <ray.mcelhinney@gmail.com>
2012-11-16 21:34:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acer::Application.config.secret_token = '8bdc95a496676d843183ec1564ab38f4073fe8e2af7a1bb1a357990db6107229c5e089978d93fbee2bc2c094b4fba7586e31e1ffc9eab3c48a3cdf3865b962a9'
